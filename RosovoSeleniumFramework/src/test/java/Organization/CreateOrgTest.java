a7f41997c4923e8fbd804ef9601afe1fd611c1bb
Chiini <Chiini@admin>
2021-03-12 17:22:06 +0545

    new

---
+++ RosovoSeleniumFramework/src/test/java/Organization/CreateOrgTest.java
@@ -0,0 +1,18 @@
+package Organization;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+
+	@Test
+	public void CreateOrgWithMobNO()
+	{
+		System.out.println("CreateOreWithMobNO");
+	}
+	
+	@Test
+	public void CreateOrg1()
+	{
+		System.out.println("CreateOrg");
+	}
+}
