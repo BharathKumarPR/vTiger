17d92d005e8b9ff965593854a97bf11c0fea17a4
Chiini <Chiini@admin>
2021-03-15 13:45:39 +0545

    modification

---
+++ RosovoSeleniumFramework/src/test/java/OrganizationTest/CreateOrgTest.java
@@ -0,0 +1,15 @@
+package OrganizationTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+
+	@Test
+	public void Createcontact()
+	{
+		System.out.println("Createcontact");
+	}
+	
+}
